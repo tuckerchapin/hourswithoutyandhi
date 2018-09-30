7bbe7d9ba07c2971a3515aeec413ce2cdaffcacf
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-09-30 18:45:34 -0400

    Working on text formating.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,64 @@
 import React from 'react';
 
-import s from '../styles/app.style';
-import Billboard from '../images/blank_billboard.jpg';
+import Sky from '../images/sky.jpg';
+import BillboardTop from '../images/billboard_top.png';
+import BillboardBase from '../images/billboard_base.png';
+
+
 
 export default function App() {
   return (
     <div style={{
-        backgroundImage: `url(${Billboard})`,
+        backgroundImage: `url(${Sky})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center top',
+        backgroundPosition: 'center center',
         width: "100%",
         height: "100%",
     }}>
-        Hello
+        <div style={{
+            width: '80%',
+            position: 'fixed',
+            bottom: '0px',
+            margin: '0% 10%',
+        }}>
+            {/* <img
+                style={{
+                    width: '50%',
+                    margin: '0% 25%',
+                }}
+                src={BillboardTop}
+            /> */}
+            <div style={{
+                background: "#FFF",
+                padding: "10%",
+            }}>
+                <blockquote className="chat">
+                    <p className="me">
+                        YANDHI 9 29 18
+                    </p>
+
+                    <p className="them">
+                        Where is Yandhi?
+                    </p>
+
+                    <p className="them">
+                        It's been N hours.
+                    </p>
+
+                    {/* <p className="them">
+                        And you've tweeted N times without mentioning it.
+                    </p> */}
+                </blockquote>
+            </div>
+            <img
+                style={{
+                    bottom: '-10px',
+                    width: '100%'
+                }}
+                src={BillboardBase}
+            />
+        </div>
     </div>
   );
 }
