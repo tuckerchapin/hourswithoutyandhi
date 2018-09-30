d0e39573ca7262000a4d31da689ae35af768373a
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-09-30 17:24:27 -0400

    Test bg image.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import s from '../styles/app.style';
 export default function App() {
   return (
     <div style={{
-        backgroundImage: 'url("https://raw.githubusercontent.com/tuckerchapin/hourswithoutyandhi/gh-pages/blank%20billboard.png"")',
+        backgroundImage: 'url("https://raw.githubusercontent.com/tuckerchapin/hourswithoutyandhi/gh-pages/blank%20billboard.png")',
         width: "100%",
         height: "100%",
     }}>
